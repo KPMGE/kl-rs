9f65c75783c50a5f6826c4c08adb307a5ec78972
KPMGE <jesuskevin254@gmail.com>
2024-02-15 05:02:41 -0300

    :white_check_mark: ensure lexer parsers code with identifiers

--- tests/lexer.rs
+++ tests/lexer.rs
@@ -52,3 +52,15 @@ fn given_code_with_keywords_it_should_parse_correctly() {
         assert!(token == *expected_token);
     });
 }
+
+#[test]
+fn given_code_with_identifiers_it_should_parse_correctly() {
+    let code = "testident";
+
+    let mut lexer = Lexer::new(code.to_string());
+
+    let token = lexer.next_token();
+    let expected_token = Token::Identifier(code.to_string());
+
+    assert!(token == expected_token);
+}
