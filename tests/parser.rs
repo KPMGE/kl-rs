11ac5b955f0ba006935e06567321f80588e32f7d
KPMGE <jesuskevin254@gmail.com>
2024-02-16 23:02:12 -0300

    :white_check_mark: ensure return statements with integets are parsed correctly

--- tests/parser.rs
+++ tests/parser.rs
@@ -37,3 +37,34 @@ fn given_let_statements_with_single_integers_shold_parse_correctly() {
             _ => panic!("wrong statement!"),
         })
 }
+
+#[test]
+fn given_return_statements_with_single_integers_shold_parse_correctly() {
+    let code = "
+        return 10;\
+        return 20;\
+        return 30;\
+    ";
+
+    let lexer = Lexer::new(code.to_string());
+    let mut parser = Parser::new(lexer);
+    let expected_ints = &["10", "20", "30"];
+
+    let parsed_program = parser.parse_program();
+
+    assert_eq!(parsed_program.statements.len(), 3);
+    assert_eq!(parser.errors.len(), 0);
+
+    parsed_program
+        .statements
+        .iter()
+        .enumerate()
+        .for_each(|(idx, statement)| match statement {
+            Statement::ReturnStatement { value, .. } => {
+                let expected_int = expected_ints[idx];
+
+                assert_eq!(expected_int, value.get_token_literal());
+            }
+            _ => panic!("wrong statement!"),
+        })
+}
