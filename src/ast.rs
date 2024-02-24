55438e0bee92fc2c8df9889a5af9bec52992d22b
KPMGE <jesuskevin254@gmail.com>
2024-02-24 01:34:00 -0300

    :fire: remove unused depency

--- src/ast.rs
+++ src/ast.rs
@@ -1,8 +1,3 @@
-use downcast_rs::{Downcast, impl_downcast};
-
 pub trait AstNode {
     fn get_token_literal(&self) -> String;
 }
-
-pub trait Expression: AstNode + Downcast {}
-impl_downcast!(Expression);
