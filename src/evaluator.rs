1404777fa9ba948343933764f656281bb0457dcc
KPMGE <jesuskevin254@gmail.com>
2024-03-09 11:28:15 -0300

    :sparkles: evaluate bang expressions

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -20,9 +20,28 @@ impl Evaluator {
                     token: Token::Int(value),
                 } => Object::Integer(value.parse::<i32>().expect("Could not parse integer")),
                 Expression::Boolean { value, .. } => Object::Boolean(value),
+                Expression::Prefix { operator, right } => {
+                    let right = self.eval(AstNode::Expression(*right));
+                    self.eval_prefix_expression(operator, right)
+                }
                 _ => todo!(),
             },
             AstNode::Statement(_) => todo!(),
+        }
+    }
+
+    fn eval_prefix_expression(&self, operator: Token, right: Object) -> Object {
+        match operator {
+            Token::Bang => self.eval_bang_expression(right),
+            _ => Object::Null,
+        }
+    }
+
+    fn eval_bang_expression(&self, right: Object) -> Object {
+        match right {
+            Object::Boolean(value) => Object::Boolean(!value),
+            Object::Null => Object::Boolean(true),
+            _ => Object::Boolean(false),
         }
     }
 }
