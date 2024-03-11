61492e23d87aff1b487c0d67ea3c8c900173410f
KPMGE <jesuskevin254@gmail.com>
2024-03-10 22:08:50 -0300

    :white_check_mark: ensure evaluator works correctly with if-else expressions

--- tests/evaluator.rs
+++ tests/evaluator.rs
@@ -1,9 +1,5 @@
-use kl_rs::evaluator::{Object, Evaluator};
-use kl_rs::{
-    ast::AstNode,
-    lexer::Lexer,
-    parser::Parser,
-};
+use kl_rs::evaluator::{Evaluator, Object};
+use kl_rs::{ast::AstNode, lexer::Lexer, parser::Parser};
 
 #[test]
 fn given_an_integer_expression_it_should_evaluate_to_the_right_object() {
@@ -16,7 +12,7 @@ fn given_an_integer_expression_it_should_evaluate_to_the_right_object() {
         let parsed_program = parser.parse_program();
         let node = match parsed_program {
             AstNode::Program { statements } => statements.first().unwrap().clone(),
-            _ => panic!("Unexpected AstNode!")
+            _ => panic!("Unexpected AstNode!"),
         };
 
         let evaluator = Evaluator {};
@@ -24,7 +20,7 @@ fn given_an_integer_expression_it_should_evaluate_to_the_right_object() {
 
         assert_eq!(evaluated_obj, *expected_objects.get(idx).unwrap());
     })
-} 
+}
 
 #[test]
 fn given_boolean_expressions_it_should_evaluate_to_the_right_object() {
@@ -37,7 +33,7 @@ fn given_boolean_expressions_it_should_evaluate_to_the_right_object() {
         let parsed_program = parser.parse_program();
         let node = match parsed_program {
             AstNode::Program { statements } => statements.first().unwrap().clone(),
-            _ => panic!("Unexpected AstNode!")
+            _ => panic!("Unexpected AstNode!"),
         };
 
         let evaluator = Evaluator {};
@@ -55,7 +51,7 @@ fn given_prefix_expressions_it_should_evaluate_correctly() {
         Object::Boolean(true),
         Object::Boolean(true),
         Object::Integer(-10),
-        Object::Null
+        Object::Null,
     ];
 
     test_codes.iter().enumerate().for_each(|(idx, code)| {
@@ -64,7 +60,42 @@ fn given_prefix_expressions_it_should_evaluate_correctly() {
         let parsed_program = parser.parse_program();
         let node = match parsed_program {
             AstNode::Program { statements } => statements.first().unwrap().clone(),
-            _ => panic!("Unexpected AstNode!")
+            _ => panic!("Unexpected AstNode!"),
+        };
+
+        let evaluator = Evaluator {};
+        let evaluated_obj = evaluator.eval(node);
+
+        assert_eq!(evaluated_obj, *expected_objects.get(idx).unwrap());
+    })
+}
+
+#[test]
+fn given_if_else_expressions_it_should_evaluate_correctly() {
+    let test_codes = vec![
+        "if (1 < 2) { 10 } else { 20 };",
+        "if (true) { 10 } else { 20 };",
+        "if (false) { 10 } else { 20 };",
+        "if (200) { 10 } else { 20 };",
+        "if (0) { 10 } else { 20 };",
+        "if (false) { 10 };",
+    ];
+    let expected_objects = vec![
+        Object::Integer(10),
+        Object::Integer(10),
+        Object::Integer(20),
+        Object::Integer(10),
+        Object::Integer(20),
+        Object::Null,
+    ];
+
+    test_codes.iter().enumerate().for_each(|(idx, code)| {
+        let lexer = Lexer::new(code.to_string());
+        let mut parser = Parser::new(lexer);
+        let parsed_program = parser.parse_program();
+        let node = match parsed_program {
+            AstNode::Program { statements } => statements.first().unwrap().clone(),
+            _ => panic!("Unexpected AstNode!"),
         };
 
         let evaluator = Evaluator {};
