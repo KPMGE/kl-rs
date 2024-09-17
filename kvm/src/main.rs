c2b620efed0ee226bfd2b62b2bdcbe752f0a9520
KPMGE <jesuskevin254@gmail.com>
2024-09-16 22:35:07 -0300

    :sparkles: implement jump instruction

--- kvm/src/main.rs
+++ kvm/src/main.rs
@@ -1,13 +1,14 @@
 use std::error::Error;
 use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Instruction {
-    Push(i32),
     Add,
     Sub,
     Div,
     Mul,
+    Push(i32),
+    Jmp(usize),
 }
 
 const STACK_CAPACITY: usize = 1;
@@ -25,51 +26,68 @@ enum KvmError {
 struct Kvm {
     stack: Vec<i32>,
     program: Vec<Instruction>,
+    ip: usize,
 }
-
 impl Kvm {
     fn new() -> Self {
         Kvm {
             stack: Vec::with_capacity(STACK_CAPACITY),
             program: Vec::new(),
+            ip: 0,
         }
     }
 
-    fn execute_program(&mut self) -> Result<(), KvmError> {
-        for inst in self.program.iter() {
-            match inst {
-                Instruction::Push(n) => {
-                    if self.stack.len() >= STACK_CAPACITY {
-                        return Err(KvmError::StackOverflow);
-                    }
-                    self.stack.push(*n);
+    fn execute_instruction(&mut self, inst: Instruction) -> Result<(), KvmError> {
+        match inst {
+            Instruction::Push(n) => {
+                if self.stack.len() >= STACK_CAPACITY {
+                    return Err(KvmError::StackOverflow);
                 }
-                Instruction::Add => {
-                    let n1 = self.stack.pop().ok_or_else(|| KvmError::StackUnderflow)?;
-                    let n2 = self.stack.pop().ok_or_else(|| KvmError::StackUnderflow)?;
-                    self.stack.push(n1 + n2);
-                }
-                Instruction::Sub => {
-                    let n1 = self.stack.pop().ok_or_else(|| KvmError::StackUnderflow)?;
-                    let n2 = self.stack.pop().ok_or_else(|| KvmError::StackUnderflow)?;
-                    self.stack.push(n1 - n2);
-                }
-                Instruction::Div => {
-                    let n1 = self.stack.pop().ok_or_else(|| KvmError::StackUnderflow)?;
-                    let n2 = self.stack.pop().ok_or_else(|| KvmError::StackUnderflow)?;
-
-                    if n2 == 0 {
-                        return Err(KvmError::DivisionByZero);
-                    }
-
-                    self.stack.push(n1 / n2);
-                }
-                Instruction::Mul => {
-                    let n1 = self.stack.pop().ok_or_else(|| KvmError::StackUnderflow)?;
-                    let n2 = self.stack.pop().ok_or_else(|| KvmError::StackUnderflow)?;
-                    self.stack.push(n1 * n2);
-                }
+                self.stack.push(n);
+                self.ip += 1;
             }
+            Instruction::Add => {
+                let n1 = self.stack.pop().ok_or_else(|| KvmError::StackUnderflow)?;
+                let n2 = self.stack.pop().ok_or_else(|| KvmError::StackUnderflow)?;
+                self.stack.push(n1 + n2);
+                self.ip += 1;
+            }
+            Instruction::Sub => {
+                let n1 = self.stack.pop().ok_or_else(|| KvmError::StackUnderflow)?;
+                let n2 = self.stack.pop().ok_or_else(|| KvmError::StackUnderflow)?;
+                self.stack.push(n1 - n2);
+                self.ip += 1;
+            }
+            Instruction::Div => {
+                let n1 = self.stack.pop().ok_or_else(|| KvmError::StackUnderflow)?;
+                let n2 = self.stack.pop().ok_or_else(|| KvmError::StackUnderflow)?;
+
+                if n2 == 0 {
+                    return Err(KvmError::DivisionByZero);
+                }
+
+                self.stack.push(n1 / n2);
+                self.ip += 1;
+            }
+            Instruction::Mul => {
+                let n1 = self.stack.pop().ok_or_else(|| KvmError::StackUnderflow)?;
+                let n2 = self.stack.pop().ok_or_else(|| KvmError::StackUnderflow)?;
+                self.stack.push(n1 * n2);
+                self.ip += 1;
+            }
+            Instruction::Jmp(addr) => {
+                self.ip = addr;
+            }
+        };
+
+        Ok(())
+    }
+
+    fn execute_program(&mut self) -> Result<(), KvmError> {
+        let n = self.program.len();
+        for _ in 0..n {
+            let inst = self.program.get(self.ip).unwrap();
+            self.execute_instruction(inst.clone())?;
         }
 
         Ok(())
@@ -95,14 +113,8 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let prog = vec![Instruction::Push(1), Instruction::Push(3), Instruction::Add];
 
-    vm.dump_stack();
     vm.load_program(prog);
-
-    println!("------");
-
-    vm.dump_stack();
     vm.execute_program()?;
-
     vm.dump_stack();
 
     Ok(())
