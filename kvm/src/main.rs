f49bd8a55a06138876f34a71fcb7b0de2abb7d81
KPMGE <jesuskevin254@gmail.com>
2024-09-16 21:36:45 -0300

    :recycle: refactor vm

--- kvm/src/main.rs
+++ kvm/src/main.rs
@@ -6,29 +6,64 @@ enum Instruction {
 
 const STACK_CAPACITY: usize = 1024;
 
-fn main() {
-    let mut prog_stack: Vec<i32> = Vec::with_capacity(STACK_CAPACITY);
+struct Kvm {
+    stack: Vec<i32>,
+    program: Vec<Instruction>,
+}
 
-    let prog = vec![Instruction::Push(1), Instruction::Push(3), Instruction::Add];
-
-    for inst in &prog {
-        println!("{:#?}", inst);
+impl Kvm {
+    fn new() -> Self {
+        Kvm {
+            stack: Vec::with_capacity(STACK_CAPACITY),
+            program: Vec::new(),
+        }
     }
 
-    for inst in &prog {
-        match inst {
-            Instruction::Push(n) => prog_stack.push(*n),
-            Instruction::Add => {
-                let n1 = prog_stack.pop().unwrap();
-                let n2 = prog_stack.pop().unwrap();
-                prog_stack.push(n1 + n2);
+    fn execute_program(&mut self) {
+        for inst in self.program.iter() {
+            match inst {
+                Instruction::Push(n) => {
+                    self.stack.push(*n);
+                }
+                Instruction::Add => {
+                    let n1 = self.stack.pop().unwrap();
+                    let n2 = self.stack.pop().unwrap();
+                    self.stack.push(n1 + n2);
+                }
             }
         }
     }
 
-    for e in &prog_stack {
-        println!("{:#?}", e);
+    fn load_program(&mut self, prog: Vec<Instruction>) {
+        self.program.extend(prog);
     }
 
-    println!("Hello, world!");
+    fn dump_stack(&self) {
+        println!("Stack: ");
+
+        if self.stack.is_empty() {
+            println!("[Empty]");
+        } else {
+            self.stack.iter().for_each(|e| println!("{}", e));
+        }
+    }
+}
+
+
+fn main() {
+    let mut vm = Kvm::new();
+
+    let prog = vec![Instruction::Push(1), Instruction::Push(3), Instruction::Add];
+
+    vm.dump_stack();
+    vm.load_program(prog);
+
+    println!("------");
+
+    vm.dump_stack();
+    vm.execute_program();
+
+    println!("------");
+
+    vm.dump_stack();
 }
