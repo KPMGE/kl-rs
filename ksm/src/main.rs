abbcb33587da3238f0e81867e52bda3750c1b841
KPMGE <jesuskevin254@gmail.com>
2024-09-20 21:42:03 -0300

    :sparkles: implement disassembler

--- ksm/src/main.rs
+++ ksm/src/main.rs
@@ -1,26 +1,43 @@
-use kvm::Instruction;
-use std::env;
+use kvm::{Instruction, Kvm};
 use std::error::Error;
 
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    input_file: String,
+
+    #[arg(short, long)]
+    output_file: Option<String>,
+
+    #[arg(short, long)]
+    disassemble: bool,
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let args: Vec<String> = env::args().collect();
+    let args = Args::parse();
 
-    if args.len() < 3 {
-        panic!("Usage: ksm <input.ksm> <output.kvm>");
+    if args.disassemble {
+        let mut vm = Kvm::new();
+        vm.load_program_from_file(&args.input_file);
+        let instructions = vm.get_instructions();
+        instructions.iter().for_each(|inst| println!("{}", inst));
+    } else {
+        let prog_asm = std::fs::read_to_string(args.input_file)?;
+        let prog_inst: Vec<Instruction> = prog_asm
+            .lines()
+            .map(|line| line.trim().try_into().unwrap())
+            .collect();
+
+        println!("prog: {:?}", prog_inst);
+        // TODO: handle option without expect
+        kvm::save_program_to_file(
+            &prog_inst,
+            &args.output_file.expect("Expected output file!"),
+        );
     }
-
-    let input_file = &args[1];
-    let output_file = &args[2];
-
-    let prog_asm = std::fs::read_to_string(input_file)?;
-    let prog: Vec<Instruction> = prog_asm
-        .lines()
-        .map(|line| line.trim().try_into().unwrap())
-        .collect();
-
-    println!("prog: {:?}", prog);
-
-    kvm::save_program_to_file(prog, output_file);
 
     Ok(())
 }
